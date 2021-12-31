92930b2fe5329edbeb802dfd0413563f458c0e5f
Rafaël L. Bekkema <rafael@rafaelbekkema.nl>
2021-12-31 02:09:57 +0100

    Day 4

--- main.go
+++ main.go
@@ -3,11 +3,71 @@ package main
 import (
 	"fmt"
 	"log"
+	"sort"
 	"strconv"
 	"strings"
 
 	"github.com/rafael-luigi-bekkema/advent-of-code-2016/set"
 )
+
+func day4b(input string, sid int) string {
+	chars := []byte(strings.ReplaceAll(input, "-", " "))
+	for i := 0; i < sid; i++ {
+		for j, c := range chars {
+			if c == ' ' {
+				continue
+			}
+			if c == 'z' {
+				chars[j] = 'a'
+				continue
+			}
+			chars[j]++
+		}
+	}
+	return string(chars)
+}
+
+func day4a(input []string) (int, int) {
+	checksum := func(inp string) string {
+		cc := map[rune]int{}
+		var chars []rune
+		for _, c := range inp {
+			if c == '-' {
+				continue
+			}
+			if _, ok := cc[c]; !ok {
+				chars = append(chars, c)
+			}
+			cc[c]++
+		}
+		sort.Slice(chars, func(i, j int) bool {
+			if cc[chars[i]] == cc[chars[j]] {
+				return chars[i] < chars[j]
+			}
+			return cc[chars[i]] > cc[chars[j]]
+		})
+		return string(chars[:5])
+	}
+
+	var total int
+	var partBid int
+	for _, line := range input {
+		lidx := strings.LastIndex(line, "-")
+		csum := checksum(line[:lidx])
+
+		var sid int
+		var sum string
+		fmt.Sscanf(line[lidx:], "-%d[%s]", &sid, &sum)
+		decoded := day4b(line[:lidx], sid)
+		if decoded == "northpole object storage" {
+			partBid = sid
+		}
+		if sum[:len(sum)-1] == csum {
+			total += sid
+		}
+	}
+	return total, partBid
+}
 
 func day3b(input [][3]int) int {
 	var count int
--- main_test.go
+++ main_test.go
@@ -4,6 +4,19 @@ import (
 	"testing"
 )
 
+func TestDay4a(t *testing.T) {
+	result, _ := day4a([]string{
+		"aaaaa-bbb-z-y-x-123[abxyz]",
+		"a-b-c-d-e-f-g-h-987[abcde]",
+		"not-a-real-room-404[oarel]",
+		"totally-real-room-200[decoy]"})
+	TestEqual(t, 1515, result)
+	result, resultID := day4a(Lines(4))
+	TestEqual(t, 245102, result)
+	TestEqual(t, "very encrypted name", day4b("qzmt-zixmtkozy-ivhz", 343))
+	TestEqual(t, 324, resultID)
+}
+
 func TestDay3(t *testing.T) {
 	TestEqual(t, 2, day3a([][3]int{{5, 6, 4}, {5, 10, 25}, {5, 5, 5}}))
 	input := day3input()
