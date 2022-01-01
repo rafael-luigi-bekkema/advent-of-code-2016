9059cf44128227f4d23f5129e6eff2202c1fcd1d
Rafaël L. Bekkema <rafael@rafaelbekkema.nl>
2022-01-01 04:12:25 +0100

    Day 9

--- main.go
+++ main.go
@@ -11,6 +11,39 @@ import (
 	"github.com/rafael-luigi-bekkema/advent-of-code-2016/set"
 )
 
+func day9decompressB(input string) int {
+	return day9decompress(input, true, 0)
+}
+
+func day9decompressA(input string) int {
+	return day9decompress(input, false, 0)
+}
+
+func day9decompress(input string, recursive bool, depth int) int {
+	var chunk string
+	var ok bool
+	var length, n int
+	var count int
+	for {
+		chunk, input, ok = strings.Cut(input, "(")
+		count += len(chunk)
+		if !ok {
+			break
+		}
+		chunk, input, ok = strings.Cut(input, ")")
+		fmt.Sscanf(chunk, "%dx%d", &length, &n)
+
+		chunk = input[:length]
+		chunkLen := len(chunk)
+		if recursive {
+			chunkLen = day9decompress(chunk, true, depth+1)
+		}
+		count += n * chunkLen
+		input = input[length:]
+	}
+	return count
+}
+
 func day8(input []string, w, h int) (int, string) {
 	display := make([][]bool, w)
 	for y := 0; y < h; y++ {
