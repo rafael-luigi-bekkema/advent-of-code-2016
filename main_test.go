1077cbdceef2952b5c5feb337e1fdd1f43d6de26
Rafaël L. Bekkema <rafael@rafaelbekkema.nl>
2022-01-04 16:47:41 +0100

    Day 13

--- main_test.go
+++ main_test.go
@@ -6,6 +6,14 @@ import (
 	"testing"
 )
 
+func TestDay13(t *testing.T) {
+	testa, _ := day13a(10, Coord{7, 4})
+	TestEqual(t, 11, testa)
+	a, b := day13a(1358, Coord{31, 39})
+	TestEqual(t, 96, a)
+	TestEqual(t, 141, b)
+}
+
 func TestDay12(t *testing.T) {
 	TestEqual(t, 42, day12a([]string{"cpy 41 a",
 		"inc a",
