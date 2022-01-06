13cfe85fa60dadd0ca119ad48d720ad54a0198b4
Rafaël L. Bekkema <rafael@rafaelbekkema.nl>
2022-01-06 11:01:53 +0100

    Day 16

--- main.go
+++ main.go
@@ -12,6 +12,37 @@ import (
 	"github.com/rafael-luigi-bekkema/advent-of-code-2016/set"
 )
 
+func day16a(length int, initial string) string {
+	flip := func(i byte) byte {
+		if i == '0' { return '1' }
+		return '0'
+	}
+	a := initial
+	for len(a) < length {
+		b := []byte(a)
+		for i, j := 0, len(b)-1; i <= j; i, j = i+1, j-1 {
+			b[i], b[j] = flip(b[j]), flip(b[i])
+		}
+		a = fmt.Sprintf("%s0%s", a, b)
+	}
+	cs := a[:length]
+	for {
+		ncs := make([]byte, len(cs)/2)
+		for i := 0; i < len(cs); i += 2 {
+			if cs[i] == cs[i+1] {
+				ncs[i/2] = '1'
+			} else {
+				ncs[i/2] = '0'
+			}
+		}
+		cs = string(ncs)
+		if len(cs)%2 != 0 {
+			break
+		}
+	}
+	return cs
+}
+
 func day15b(input []string) int {
 	return day15(input, true)
 }
@@ -35,9 +66,9 @@ func day15(input []string, b bool) int {
 	}
 	if b {
 		discs = append(discs, disc{
-			nr: len(discs)+1,
+			nr:        len(discs) + 1,
 			positions: 11,
-			startPos: 0,
+			startPos:  0,
 		})
 	}
 	count := 0
--- main_test.go
+++ main_test.go
@@ -6,6 +6,12 @@ import (
 	"testing"
 )
 
+func TestDay16(t *testing.T) {
+	TestEqual(t, "01100", day16a(20, "10000"))
+	TestEqual(t, "01110011101111011", day16a(272, "11110010111001001"))
+	TestEqual(t, "11001111011000111", day16a(35651584, "11110010111001001"))
+}
+
 func TestDay15(t *testing.T) {
 	TestEqual(t, 5, day15a([]string{
 		"Disc #1 has 5 positions; at time=0, it is at position 4.",
