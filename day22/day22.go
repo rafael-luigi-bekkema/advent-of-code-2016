f900ea136b882433df53999dab63f83990747f7b
Rafaël L. Bekkema <rafael@rafaelbekkema.nl>
2022-01-10 16:51:24 +0100

    wip

--- day22/day22.go
+++ day22/day22.go
@@ -1,19 +1,28 @@
 package day22
 
-import "fmt"
+import (
+	"fmt"
 
-func day22a(input []string) int {
-	type Node struct {
-		x, y                   int
-		size, used, avail, use int
-	}
+	"github.com/rafael-luigi-bekkema/advent-of-code-2016/util"
+)
+
+type Node struct {
+	x, y                   int
+	size, used, avail, use int
+}
+
+func parseNodes(input []string) []Node {
 	nodes := make([]Node, len(input)-2)
 	for i, line := range input[2:] {
 		n := &nodes[i]
 		fmt.Sscanf(line, "/dev/grid/node-x%d-y%d %dT %dT %dT %d%%",
 			&n.x, &n.y, &n.size, &n.used, &n.avail, &n.use)
-		fmt.Println(*n)
 	}
+	return nodes
+}
+
+func day22a(input []string) int {
+	nodes := parseNodes(input)
 	var total int
 	for i, n1 := range nodes {
 		for _, n2 := range nodes[i+1:] {
@@ -22,5 +31,51 @@ func day22a(input []string) int {
 			}
 		}
 	}
+	return total
+}
+
+func day22b(input []string) int {
+	nodes := parseNodes(input)
+	last := &nodes[len(nodes)-1]
+	height := last.y + 1
+	width := last.x + 1
+	target := &nodes[last.x*height]
+	tsize := target.used
+
+	var empty func(node *Node, hist [][2]int, moves int) int
+	empty = func(node *Node, hist [][2]int, moves int) int {
+		for _, offs := range [][2]int{{0, -1}, {1, 0}, {0, 1}, {-1, 0}} {
+			dx, dy := node.x+offs[0], node.y+offs[1]
+			if dx < 0 || dx > width-1 || dy < 0 || dy > height-1 {
+				continue
+			}
+			if util.In([2]int{dx, dy}, hist...) {
+				continue
+			}
+			to := &nodes[dx*height+dy]
+			if to.avail > node.used {
+
+			} else {
+				empty(to, append(hist, [2]int{dx, dy}), moves+1)
+			}
+		}
+		node.used = 0
+		return moves
+	}
+
+	_ = width
+	_ = tsize
+	var total int
+	for x := target.x; x > 0; x-- {
+		to := &nodes[(x-1)*height]
+		if to.avail < tsize {
+			total += empty(to, [][2]int{{target.x, target.y}}, 0)
+		}
+		total++
+		target.used = 0
+		to.used += tsize
+		target = to
+	}
+
 	return total
 }
--- day22/day22_test.go
+++ day22/day22_test.go
@@ -1,6 +1,7 @@
 package day22
 
 import (
+	"fmt"
 	"testing"
 
 	. "github.com/rafael-luigi-bekkema/advent-of-code-2016/util"
@@ -8,4 +9,20 @@ import (
 
 func TestDay22(t *testing.T) {
 	TestEqual(t, 937, day22a(Lines(22)))
+
+	example := []string{"",
+		"Filesystem            Size  Used  Avail  Use%",
+		"/dev/grid/node-x0-y0   10T    8T     2T   80%",
+		"/dev/grid/node-x0-y1   11T    6T     5T   54%",
+		"/dev/grid/node-x0-y2   32T   28T     4T   87%",
+		"/dev/grid/node-x1-y0    9T    7T     2T   77%",
+		"/dev/grid/node-x1-y1    8T    0T     8T    0%",
+		"/dev/grid/node-x1-y2   11T    7T     4T   63%",
+		"/dev/grid/node-x2-y0   10T    6T     4T   60%",
+		"/dev/grid/node-x2-y1    9T    8T     1T   88%",
+		"/dev/grid/node-x2-y2    9T    6T     3T   66%",
+	}
+	TestEqual(t, 7, day22b(example))
+	fmt.Println("--")
+	TestEqual(t, 1, day22b(Lines(22)))
 }
